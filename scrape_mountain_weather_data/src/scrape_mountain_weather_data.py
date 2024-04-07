cd392e04c6036571f8cf9bafbab68e1eccee1ec4
pmwaddell <pmwaddell9@gmail.com>
2024-04-07 10:30:16 -0400

    Update backup .csv files for our data

--- scrape_mountain_weather_data/src/scrape_mountain_weather_data.py
+++ scrape_mountain_weather_data/src/scrape_mountain_weather_data.py
@@ -378,5 +378,5 @@ def scrape_weather(mtns_to_elevs, current_only=False):
                 dfs.append(
                     scrape_mtn_full_forecast_table_at_elev(mtn_name, elev))
             print('complete.')
-            time.sleep(5)
+            time.sleep(7)
     return pd.concat(dfs, ignore_index=True)
